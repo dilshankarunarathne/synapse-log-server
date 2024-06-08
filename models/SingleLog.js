1a53bbab79363843a7dc2af7099cf366b851906f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-09 03:05:10 +0530

    net logger model

---
+++ models/SingleLog.js
@@ -0,0 +1,11 @@
+const mongoose = require('../middleware/dbConnectionMiddleware');
+
+const LogSchema = new mongoose.Schema({
+  timestamp: { type: Date, default: Date.now },
+  clientId: String,
+  jobId: String,
+  logLevel: String,
+  logMessage: String,
+});
+
+module.exports = mongoose.model('Log', LogSchema);

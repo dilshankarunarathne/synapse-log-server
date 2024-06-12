d5f0fe9cf0e59e182609ba1530482ac477e5f7ee
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:13:36 +0530

    client log route

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 const db = require('./middleware/dbConnectionMiddleware');
 
 const networkController = require('./routes/networkController');
+const singleClientController = require('./routes/singleClientController');
 
 app.use(cors()); 
 app.use(express.json());
 
 app.use('/netlog', networkController);
+app.use('/clientlog', singleClientController);
 
 app.listen(3000, () => console.log('Server started on port 3000'));

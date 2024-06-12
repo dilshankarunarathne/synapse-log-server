fadaf794d80efee563e5bd7ec62c0b35aa246481
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:15:02 +0530

    Update singleClientController.js

--- routes/singleClientController.js
+++ routes/singleClientController.js
@@ -3,11 +3,13 @@ const authMiddleware = require('../middleware/authMiddleware');
 const express = require('express');
 const router = express.Router();
 
-router.post('/db-log', authMiddleware, (req, res) => {
+// TODO: implement auth handling
+
+router.post('/db-log', (req, res) => {
     res.send('Logging was successful'); // TODO: Implement logging
 });
 
-router.post('/flat-log', authMiddleware, (req, res) => {
+router.post('/flat-log', (req, res) => {
     res.send('Logging was successful'); // TODO: Implement logging
 });
 

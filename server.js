24452d2da886cc68aec408e7c5db3214a3fbea17
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:12:43 +0530

    netlog route

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const cors = require('cors');
 const app = express();
 const db = require('./middleware/dbConnectionMiddleware');
 
-const authController = require('./routes/authController');
-const securedController = require('./routes/networkController');
+const networkController = require('./routes/networkController');
 
 app.use(cors()); 
 app.use(express.json());
 
-app.use('/auth', authController);
-app.use('/secured', securedController);
+app.use('/netlog', networkController);
 
 app.listen(3000, () => console.log('Server started on port 3000'));

3ef2cab57922a27a7a7c2bc55dcbf95c0640d15f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:20:32 +0530

    Update networkController.js

--- routes/networkController.js
+++ routes/networkController.js
@@ -6,7 +6,17 @@ const Log = require('../models/NetLog');
 // TODO: implement auth handling
 
 router.post('/db-log', (req, res) => {
-    res.send('Logging was successful'); // TODO: Implement logging
+    const log = new Log({
+        timestamp: req.body.timestamp,
+        clientId: req.body.clientId,
+        jobId: req.body.jobId,
+        logLevel: req.body.logLevel,
+        logMessage: req.body.logMessage,
+    });
+
+    log.save()
+        .then(() => res.send('Logging was successful'))
+        .catch(err => res.status(500).send('Server error'));
 });
 
 router.post('/flat-log', (req, res) => {

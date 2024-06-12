4ac3bab33d8c35ef771a7c9c01a74860d18230e9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:16:05 +0530

    rem auth

--- routes/networkController.js
+++ routes/networkController.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // TODO: implement auth handling
 
-router.post('/db-log', authMiddleware, (req, res) => {
+router.post('/db-log', (req, res) => {
   res.send('Logging was successful'); // TODO: Implement logging
 });
 
-router.post('/flat-log', authMiddleware, (req, res) => {
+router.post('/flat-log', (req, res) => {
   res.send('Logging was successful'); // TODO: Implement logging
 });
 

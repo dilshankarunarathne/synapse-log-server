cded2ddb41c0e98cc31d9f91c8c7c6d67e384f8e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-12 21:11:57 +0530

    rem auth

--- models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('../middleware/dbConnectionMiddleware');
-
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model('User', UserSchema);

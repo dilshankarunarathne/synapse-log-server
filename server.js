cf54e7ab3c10f3510d77576bc66551b29a0aaac7
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-10-29 16:41:51 +0530

    bugfix: pretty print

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const cors = require('cors');
 const path = require('path');
 const http = require('http');
@@ -11,10 +12,15 @@ const db = require('./middleware/dbConnectionMiddleware');
 const networkController = require('./routes/networkController');
 const singleClientController = require('./routes/singleClientController');
 
-app.use(cors());
 app.use(express.json());
 
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((err, _, res, __) =>
+  res.status(err.status).json({ message: err.message })
+);
 
 app.use('/netlog', networkController);
 app.use('/clientlog', singleClientController);
@@ -30,15 +36,24 @@ wss.on('connection', (ws) => {
   console.log('WS: Client connected');
 });
 
+app.options('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+
 app.post('/log', (req, res) => {
-  const logMessage = req.body.message;
+  console.log(req.body);
   // TODO : add client id (time + client id + message)
   const final_log_message = `${new Date().toISOString()} - ${JSON.stringify(req.body.message).replaceAll("\"", "")}`;
+
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(final_log_message);
     }
-  });
+});
+  
   res.status(200).send('Log message sent to clients');
 });
 
